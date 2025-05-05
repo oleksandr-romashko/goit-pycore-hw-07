0951e6b745a963432067d04902a2bee241c0ebf5
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-05-05 18:51:02 +0200

    Add some tests for Phone class

--- src/address_book/phone.py
+++ src/address_book/phone.py
@@ -7,6 +7,7 @@ the phone number is valid on assignment or update.
 
 from field import Field
 from validators.field_validators import validate_phone_number
+from validators.errors import ValidationError
 
 
 class Phone(Field):
@@ -28,3 +29,26 @@ class Phone(Field):
 
     def _validate_phone(self, phone_number: str):
         validate_phone_number(phone_number)
+
+
+if __name__ == "__main__":
+    PHONE_NUMBER_STR = "1234567890"
+    phone_1 = Phone(PHONE_NUMBER_STR)
+    assert phone_1.value == PHONE_NUMBER_STR
+
+    try:
+        phone_2 = Phone("123")
+    except ValidationError as exc:
+        error_msg = (
+            "Invalid phone number '123'. "
+            "Expected 10 digits, optionally starting with '+'."
+        )
+        assert str(exc) == error_msg
+    else:
+        cause = (
+            "Should raise Validation error "
+            "when creating Phone object with invalid phone number value"
+        )
+        assert False, cause
+
+    print("Phone tests passed.")
