29f0a6ef7d36bb2af1859f6ab9977cb475cca85d
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-05-13 21:13:04 +0200

    Add tests to Name class

--- src/address_book/name.py
+++ src/address_book/name.py
@@ -6,6 +6,7 @@ the contact name is valid on assignment.
 """
 
 from field import Field
+from validators.errors import ValidationError
 from validators.field_validators import validate_username_length
 
 
@@ -20,3 +21,33 @@ class Name(Field):
         username = username.strip()
         validate_username_length(username)
         super().__init__(username)
+
+
+if __name__ == "__main__":
+    # TESTS
+
+    name_valid_str = "Alice"
+    name_too_short_str = "A"
+    name_too_long_str = "A" * 51
+
+    name_valid = Name(name_valid_str)
+
+    try:
+        Name(name_too_short_str)
+    except ValidationError as exc:
+        cause = f"Username '{name_too_short_str}' is too short and should have at least 2 symbols."
+        assert str(exc) == cause
+    else:
+        cause = "Should raise Validation error when name is too short"
+        assert False, cause
+
+    try:
+        Name(name_too_long_str)
+    except ValidationError as exc:
+        cause = "Username 'AAAAAAAAAAAA...' is too long and should have not more than 50 symbols."
+        assert str(exc) == cause
+    else:
+        cause = "Should raise Validation error when name is too short"
+        assert False, cause
+
+    print("Name tests passed.")
