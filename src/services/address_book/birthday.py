af205e079f8ff807082806c9eb07d95042b98094
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-05-17 00:43:30 +0200

    Update Birthday

--- src/services/address_book/birthday.py
+++ src/services/address_book/birthday.py
@@ -13,10 +13,7 @@ from utils.constants import BIRTHDAY_FORMAT_MSG
 from utils.date_utils import format_date_str
 from validators.args_validators import validate_argument_type
 from validators.errors import ValidationError
-from validators.field_validators import (
-    validate_birthday_format,
-    validate_birthday_is_in_the_past,
-)
+from validators.field_validators import validate_birthday_format
 
 
 class Birthday(Field):
@@ -27,150 +24,116 @@ class Birthday(Field):
     and on value changes.
     """
 
-    def __init__(self, value: str | date):
-        birth_date = self.__validate_date_value(value)
-        super().__init__(birth_date)
+    def __init__(self, date_value: str | date):
+        date_obj = self._validate_and_parse_date(date_value)
+        super().__init__(date_obj)
 
     def __str__(self):
         return format_date_str(self.value)
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}(value='{format_date_str(self.value)}')"
-
     @Field.value.setter
-    def value(self, new_value: str | date):
-        birth_date = self.__validate_date_value(new_value)
-        self._value = birth_date
+    def value(self, date_value: str | date):
+        """
+        Sets a new validated birthday date value.
 
-    def __validate_date_value(self, value: str | date) -> date:
-        validate_argument_type(value, (str, date))
-        birth_date = value
-        if not isinstance(value, date):
-            birth_date = validate_birthday_format(value)
-        validate_birthday_is_in_the_past(birth_date)
-        return birth_date
+        Overrides setter from parent adding validation.
+        """
+        date_obj = self._validate_and_parse_date(date_value)
+        self._value = date_obj
+
+    def _validate_and_parse_date(self, date_value: str | date) -> date:
+        validate_argument_type(date_value, (str, date))
+        if isinstance(date_value, str):
+            date_value = validate_birthday_format(date_value)
+        return date_value
 
 
 if __name__ == "__main__":
     # TESTS
 
     # Test data
-    date_str_valid = "02.03.2000"
-    date_str_with_invalid_format = "2000-03-02"
-    date_str_in_the_future = "01.01.2200"
+    TEST_DATE_STR_VALID = "02.03.2000"
+    TEST_DATE_STR_VALID_REPR = "Birthday(value=datetime.date(2000, 3, 2))"
+    TEST_DATE_STR_WITH_INVALID_FORMAT = "2000-03-02"
+    TEST_DATE_STR_IN_THE_FUTURE = "01.01.2200"
 
-    date_obj_valid = date(2000, 3, 2)
-    date_obj_in_the_future = date(2200, 1, 1)
+    test_date_obj_valid = date(2000, 3, 2)
+    test_date_obj_in_the_future = date(2200, 1, 1)
 
-    # Create instance with valid date string
-    birthday_test_1 = Birthday(date_str_valid)
-    assert isinstance(birthday_test_1.value, date)
-    assert format_date_str(birthday_test_1.value) == date_str_valid
+    # Test creation of instance with valid date string
+    test_birthday_1 = Birthday(TEST_DATE_STR_VALID)
+    assert isinstance(test_birthday_1.value, date)
+    assert format_date_str(test_birthday_1.value) == TEST_DATE_STR_VALID
 
-    # Create instance with invalid date format string
+    # Test str and repr
+    assert str(test_birthday_1) == TEST_DATE_STR_VALID
+    assert repr(test_birthday_1) == TEST_DATE_STR_VALID_REPR
+
+    # Test creation of instance with invalid date format string
     try:
-        Birthday(date_str_with_invalid_format)
+        Birthday(TEST_DATE_STR_WITH_INVALID_FORMAT)
     except ValidationError as exc:
-        cause = f"Invalid provided date format '{date_str_with_invalid_format}'."
-        tip = f"Use {BIRTHDAY_FORMAT_MSG} format."
-        error_msg = f"{cause} {tip}"
-        assert str(exc) == error_msg
-    else:
-        cause = "Should raise Validation error when birthday date has invalid format"
-        assert False, cause
-
-    # Create instance with invalid date string in the future
-    try:
-        Birthday(date_str_in_the_future)
-    except ValidationError as exc:
-        cause = (
-            f"Given birthday date '{date_str_in_the_future}' can't be in the future."
-        )
-        assert str(exc) == cause
-    else:
-        cause = "Should raise Validation error when birthday date is in the future"
-        assert False, cause
-
-    # Direct assignment to Birthday instance value a valid date string
-    birthday_test_2 = Birthday(date_str_valid)
-    birthday_test_2.value = date_str_valid
-    assert isinstance(birthday_test_2.value, date)
-    assert format_date_str(birthday_test_2.value) == date_str_valid
-
-    # Direct assignment to Birthday instance value an invalid date format string
-    birthday_test_3 = Birthday(date_str_valid)
-    try:
-        birthday_test_3.value = date_str_with_invalid_format
-    except ValidationError as exc:
-        error_msg = (
-            f"Invalid provided date format '{date_str_with_invalid_format}'. "
+        TEST_ERR_MSG_INVALID_DATE_FORMAT_WHEN_INIT = (
+            f"Invalid provided date format '{TEST_DATE_STR_WITH_INVALID_FORMAT}'. "
             f"Use {BIRTHDAY_FORMAT_MSG} format."
         )
-        assert str(exc) == error_msg
+        assert str(exc) == TEST_ERR_MSG_INVALID_DATE_FORMAT_WHEN_INIT
     else:
-        cause = (
+        assert (
+            False
+        ), "Should raise Validation error when birthday date has invalid format"
+
+    # Test creation of instance with date string in the future
+    Birthday(TEST_DATE_STR_IN_THE_FUTURE)
+
+    # Test direct assignment to Birthday instance value a valid date string
+    test_birthday_2 = Birthday(TEST_DATE_STR_VALID)
+    test_birthday_2.value = TEST_DATE_STR_VALID
+    assert isinstance(test_birthday_2.value, date)
+    assert format_date_str(test_birthday_2.value) == TEST_DATE_STR_VALID
+
+    # Test direct assignment to Birthday instance value an invalid date format string
+    test_birthday_3 = Birthday(TEST_DATE_STR_VALID)
+    try:
+        test_birthday_3.value = TEST_DATE_STR_WITH_INVALID_FORMAT
+    except ValidationError as exc:
+        TEST_ERR_MSG_INVALID_DATE_FORMAT_WHEN_ASSIGN_VALUE = (
+            f"Invalid provided date format '{TEST_DATE_STR_WITH_INVALID_FORMAT}'. "
+            f"Use {BIRTHDAY_FORMAT_MSG} format."
+        )
+        assert str(exc) == TEST_ERR_MSG_INVALID_DATE_FORMAT_WHEN_ASSIGN_VALUE
+    else:
+        assert False, (
             "Should raise Validation error "
             "when updating Birthday instance value directly with invalid date string value"
         )
-        assert False, cause
-    assert isinstance(birthday_test_3.value, date)
-    assert format_date_str(birthday_test_3.value) == date_str_valid
+    assert isinstance(test_birthday_3.value, date)
+    assert format_date_str(test_birthday_3.value) == TEST_DATE_STR_VALID
 
-    # Direct assignment to Birthday instance value an invalid date string in the future
-    birthday_test_4 = Birthday(date_str_valid)
-    try:
-        birthday_test_4.value = date_str_in_the_future
-    except ValidationError as exc:
-        error_msg = (
-            f"Given birthday date '{date_str_in_the_future}' can't be in the future."
-        )
-        assert str(exc) == error_msg
-    else:
-        cause = (
-            "Should raise Validation error "
-            "when updating Birthday instance value directly "
-            "with invalid date in the future string value"
-        )
-        assert False, cause
-    assert isinstance(birthday_test_4.value, date)
-    assert format_date_str(birthday_test_4.value) == date_str_valid
+    # Test direct assignment to Birthday instance value a date string in the future
+    test_birthday_4 = Birthday(TEST_DATE_STR_VALID)
+    test_birthday_4.value = TEST_DATE_STR_IN_THE_FUTURE
+    assert isinstance(test_birthday_4.value, date)
+    assert format_date_str(test_birthday_4.value) == TEST_DATE_STR_IN_THE_FUTURE
 
-    # Create instance with valid date object
-    birthday_test_5 = Birthday(date_obj_valid)
-    assert isinstance(birthday_test_5.value, date)
-    assert format_date_str(birthday_test_5.value) == "02.03.2000"
+    # Test creation of instance with valid date object
+    test_birthday_5 = Birthday(test_date_obj_valid)
+    assert isinstance(test_birthday_5.value, date)
+    assert format_date_str(test_birthday_5.value) == TEST_DATE_STR_VALID
 
-    # Create instance with invalid date object in the future
-    try:
-        Birthday(date_obj_in_the_future)
-    except ValidationError as exc:
-        cause = "Given birthday date '01.01.2200' can't be in the future."
-        assert str(exc) == cause
-    else:
-        cause = "Should raise Validation error when birthday date is in the future"
-        assert False, cause
+    # Test creation of instance with date object in the future
+    Birthday(test_date_obj_in_the_future)
 
-    # Direct assignment to Birthday instance value a valid date object
-    birthday_test_6 = Birthday(date_obj_valid)
-    birthday_test_6.value = "02.03.2000"
-    assert isinstance(birthday_test_6.value, date)
-    assert format_date_str(birthday_test_6.value) == "02.03.2000"
+    # Test direct assignment to Birthday instance value a valid date object
+    test_birthday_6 = Birthday(test_date_obj_valid)
+    test_birthday_6.value = TEST_DATE_STR_VALID
+    assert isinstance(test_birthday_6.value, date)
+    assert format_date_str(test_birthday_6.value) == TEST_DATE_STR_VALID
 
-    # Direct assignment to Birthday instance value an invalid date object in the future
-    birthday_test_7 = Birthday(date_obj_valid)
-    try:
-        birthday_test_7.value = date_obj_in_the_future
-    except ValidationError as exc:
-        error_msg = "Given birthday date '01.01.2200' can't be in the future."
-        assert str(exc) == error_msg
-    else:
-        cause = (
-            "Should raise Validation error "
-            "when updating Birthday instance value directly "
-            "with invalid date in the future object value"
-        )
-        assert False, cause
-    assert isinstance(birthday_test_7.value, date)
-    assert format_date_str(birthday_test_7.value) == "02.03.2000"
+    # Test direct assignment to Birthday instance value a date object in the future
+    test_birthday_7 = Birthday(test_date_obj_valid)
+    test_birthday_7.value = test_date_obj_in_the_future
+    assert isinstance(test_birthday_7.value, date)
+    assert format_date_str(test_birthday_7.value) == TEST_DATE_STR_IN_THE_FUTURE
 
     print("Birthday tests passed.")
