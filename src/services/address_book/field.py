ca00ed548b8dbddf1dfd7fb23773e27d2c1adc99
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-05-16 17:19:07 +0200

    Refactor Field

--- src/services/address_book/field.py
+++ src/services/address_book/field.py
@@ -15,6 +15,7 @@ class Field:
     Base class for contact record fields.
 
     Stores a single value and provides a default string representation.
+    Instances of Field are compared based on their stored value.
     """
 
     _value: Any
@@ -23,10 +24,10 @@ class Field:
         return str(self.value)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(value='{str(self.value)}')"
+        return f"{self.__class__.__name__}(value={repr(self.value)})"
 
     @property
-    def value(self):
+    def value(self) -> Any:
         """
         Retrieves the stored value of the field.
 
@@ -40,24 +41,26 @@ class Field:
         return self._value
 
     @value.setter
-    def value(self, new_value):
+    def value(self, new_value: Any) -> None:
         self._value = new_value
 
 
 if __name__ == "__main__":
     # TESTS
 
-    TEST_VALUE_STR = "value"
+    TEST_VALUE_STR = "some_value"
     TEST_VALUE_DATE = 42
 
     test_field_str = Field(TEST_VALUE_STR)
     assert isinstance(test_field_str.value, str)
     assert str(test_field_str) == TEST_VALUE_STR
-    assert test_field_str == Field(TEST_VALUE_STR)
+    assert test_field_str == Field(TEST_VALUE_STR)  # __eq__ override test
+    assert repr(test_field_str) == "Field(value='some_value')"  # __repr__ override test
 
-    test_field_date = Field(TEST_VALUE_DATE)
-    assert isinstance(test_field_date.value, int)
-    assert str(test_field_date) == "42"
-    assert test_field_date == Field(TEST_VALUE_DATE)
+    test_field_int = Field(TEST_VALUE_DATE)
+    assert isinstance(test_field_int.value, int)
+    assert str(test_field_int) == str(TEST_VALUE_DATE)
+    assert test_field_int == Field(TEST_VALUE_DATE)  # __eq__ override test
+    assert repr(test_field_int) == "Field(value=42)"  # __repr__ override test
 
     print("Field tests passed.")
